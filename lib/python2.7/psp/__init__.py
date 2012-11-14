8a26015989ff5eaf567dedb13d2817cd513f1b99
Jan Stavel <stavel.jan@gmail.com>
2012-11-14 16:41:44 +0100

    doplnil jsem osetreni toho, ze v zip souboru neni spravne pojmenovany adresar.

--- lib/python2.7/psp/__init__.py
+++ lib/python2.7/psp/__init__.py
@@ -9,6 +9,9 @@ from .amdspec import AmdSpec
 class NoMetsException(Exception):
     pass
 
+class NoProperDirectoryInZipFile(Exception):
+    pass
+
 class PSP(object):
     """ umí rozbalit balíček do zadaného pracovního adresáře.
     Umí poskytovat přístup k jednotlivým částem balíčku"""
@@ -38,6 +41,9 @@ class PSP(object):
         if not self._unzipped:
             self._unzip()
 
+        if not os.path.isdir(self.dirname):
+            raise NoProperDirectoryInZipFile(self.dirname)
+            
         fnames = [ff for ff in os.listdir(self.dirname) if 'METS' in ff]
         if not fnames:
             raise NoMetsException(self.dirname)
